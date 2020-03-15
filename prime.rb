ab9facb2fe022d478dcdcce2467497e0f53c8e37
Brittany Russell <sincerelybr@gmail.com>
2020-03-15 01:30:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  #if number is divisble by 2 -- false
+  #if number id divisible by 3 --> true
+  # A prime number is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers. A natural number greater than 1 that is not prime is called a composite number. For example, 5 is prime because the only ways of writing it as a product, 1 × 5 or 5 × 1, involve 5 itself.
+  if num/2 == 3%2
+    return true 
+  else num/2 == 2%2
+    return false
+  end 
+end
+
+puts prime?(-1)
